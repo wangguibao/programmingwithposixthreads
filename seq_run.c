d6f457d2777e455fcb9cf412f1951894da353054
Wang Guibao <wang_guibao@163.com>
2022-09-24 13:17:04 +0800

    Update and rename seq_print.c to seq_run.c

--- seq_run.c
+++ seq_run.c
@@ -1,18 +1,31 @@
+/*
+ * seq_print.c
+ * @author wangguibao (wang_guibao@163.com)
+ * @date 2022.09.24 13:16
+ * @brief Make two threads run in order
+ */
 #include <pthread.h>
 #include <stdio.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
+int turn = 0;
+
 void* th_func(void* arg) {
    int* num = (int*)(arg);
 
    while (1) {
        pthread_mutex_lock(&mutex);
-       pthread_cond_wait(&cond, &mutex);
+
+       while (turn != *num-1) {
+           pthread_cond_wait(&cond, &mutex);
+       }
        printf("%d\n", *num);
        fflush(stdout);
+       turn = (turn + 1) % 2;
        pthread_cond_signal(&cond);
+
        pthread_mutex_unlock(&mutex);
    }
 
